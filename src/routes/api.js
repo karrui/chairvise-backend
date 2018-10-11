c765d830f0ead85737baefe48ee27f1d40a7ccce
Lau Kar Rui <karrui.lau@gmail.com>
2018-10-11 13:25:06 +0800

    featAPI): upload API returns JSON with fileName and uploadDateTime keys

--- src/routes/api.js
+++ src/routes/api.js
@@ -13,22 +13,43 @@ router.get('/', (req, res, next) => {
 router.post('/upload', upload.single('file'), (req, res) => {
   const csvFile = req.file;
   const fileName = csvFile.originalname;
+  const { fileType } = req.body;
   let result = null;
 
-  switch (fileName) {
-    case 'review.csv':
-      result = csvHelper.getReviewInfo(csvFile);
-      break;
-    case 'author.csv':
-      result = csvHelper.getAuthorInfo(csvFile);
-      break;
-    case 'submission.csv':
-      result = csvHelper.getSubmissionInfo(csvFile);
-      break;
-    default:
-      res.sendStatus(422); // unknown file, no api to process
+  if (fileType) {
+    switch (fileType) {
+      case 'review':
+        result = csvHelper.getReviewInfo(csvFile, fileName);
+        break;
+      case 'author':
+        result = csvHelper.getAuthorInfo(csvFile, fileName);
+        break;
+      case 'submission':
+        result = csvHelper.getSubmissionInfo(csvFile, fileName);
+        break;
+      default:
+        res.sendStatus(422); // unknown file, no api to process
+    }
+  } else {
+    switch (fileName) {
+      case 'review.csv':
+        result = csvHelper.getReviewInfo(csvFile, fileName);
+        break;
+      case 'author.csv':
+        result = csvHelper.getAuthorInfo(csvFile, fileName);
+        break;
+      case 'submission.csv':
+        result = csvHelper.getSubmissionInfo(csvFile, fileName);
+        break;
+      default:
+        res.sendStatus(422); // unknown file, no api to process
+    }
   }
-  res.status(200).json(result);
+  if (result.error) {
+    res.sendStatus(422);
+  } else {
+    res.status(200).json(result);
+  }
 });
 
 router.post('/uploadv2', upload.single('file'), (req, res) => {
